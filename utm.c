bb8d078f62be7364971f223c0da63ecefc73718b
serceb03 <serceb03@gmail.com>
2025-09-07 01:18:06 +0300

    Описание изменений

--- utm.c
+++ utm.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <math.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+int main() {
+    int a, b, sum;
+    
+    printf("Hello World\n");
+    printf("write first digit: ");
+    scanf("%d", &a);
+    printf("write second digit: ");
+    scanf("%d", &b);
+    sum = a + b;
+    printf("sum: %d\n", sum);
+    
     return 0;
 }
